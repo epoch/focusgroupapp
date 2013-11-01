792b3de24d9ab24f052df7e8a49a2d4790d8da5b
Matt Thees <mthees7@gmail.com>
2013-10-31 20:44:02 -0400

    flash message for incorrect password match

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path
     else
+      @company = Company.new
       render :new
     end
   end
